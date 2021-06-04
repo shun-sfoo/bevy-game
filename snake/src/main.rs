0337a3ea4242fdfb446517d389947c7fd6341ad8
shun-sfoo <ganymede0915@gmail.com>
2021-06-05 00:06:19 +0800

    snake head food

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,3 +1,148 @@
+use bevy::core::FixedTimestep;
+use bevy::prelude::*;
+use bevy::render::pass::ClearColor;
+use rand::prelude::random;
+
+const ARENA_WIDTH: u32 = 10;
+const ARENA_HEIGHT: u32 = 10;
+// default plugins 不包含 camera  创建一个 2d camera
+// Commands is used to queue up commands to mutate the world and resources
+fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.insert_resource(Materials {
+        head_material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+        food_material: materials
+            .add(Color::rgb(random::<f32>(), random::<f32>(), random::<f32>()).into()),
+    })
+}
+
+#[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+struct Size {
+    width: f32,
+    height: f32,
+}
+
+impl Size {
+    pub fn square(x: f32) -> Self {
+        Self {
+            width: x,
+            height: x,
+        }
+    }
+}
+
+struct SnakeHead;
+struct Food;
+struct Materials {
+    head_material: Handle<ColorMaterial>,
+    food_material: Handle<ColorMaterial>,
+}
+
+fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
+    let window = windows.get_primary().unwrap();
+    for (sprite_size, mut sprite) in q.iter_mut() {
+        sprite.size = Vec2::new(
+            sprite_size.width / ARENA_WIDTH as f32 * window.width() as f32,
+            sprite_size.height / ARENA_HEIGHT as f32 * window.height() as f32,
+        );
+    }
+}
+
+fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Transform)>) {
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let title_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (title_size / 2.)
+    }
+
+    let window = windows.get_primary().unwrap();
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, ARENA_WIDTH as f32),
+            convert(pos.y as f32, window.height() as f32, ARENA_HEIGHT as f32),
+            0.0,
+        );
+    }
+}
+
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.head_material.clone(),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
+            ..Default::default()
+        })
+        .insert(SnakeHead)
+        .insert(Position { x: 3, y: 3 })
+        .insert(Size::square(0.8));
+}
+
+fn food_spwan(mut commands: Commands, materials: Res<Materials>) {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.food_material.clone(),
+            ..Default::default()
+        })
+        .insert(Food)
+        .insert(Position {
+            x: (random::<f32>() * ARENA_WIDTH as f32) as i32,
+            y: (random::<f32>() * ARENA_HEIGHT as f32) as i32,
+        })
+        .insert(Size::square(0.8));
+}
+
+// So the With type allows us to say “I want entities that have a snake head,
+// but I don’t care about the snake head component itself, just give me the transform”.
+fn snake_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut head_positions: Query<&mut Position, With<SnakeHead>>,
+) {
+    for mut pos in head_positions.iter_mut() {
+        if keyboard_input.pressed(KeyCode::Left) {
+            pos.x -= 1;
+        }
+        if keyboard_input.pressed(KeyCode::Right) {
+            pos.x += 1;
+        }
+        if keyboard_input.pressed(KeyCode::Down) {
+            pos.y -= 1;
+        }
+        if keyboard_input.pressed(KeyCode::Up) {
+            pos.y += 1;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 创建一个2d 游戏，需要很多不同的系统(system)：
+    // 创建窗口, render loop,  控制键盘数据， 控制sprites
+    // default plugins 给了我们所需的
+    App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Snake!".to_string(),
+            width: 500.0,
+            height: 500.0,
+            ..Default::default()
+        })
+        .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
+        .add_system(snake_movement.system())
+        .add_system_set_to_stage(
+            CoreStage::PostUpdate,
+            SystemSet::new()
+                .with_system(position_translation.system())
+                .with_system(size_scaling.system()),
+        )
+        .add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.0))
+                .with_system(food_spwan.system()),
+        )
+        .add_plugins(DefaultPlugins)
+        .run();
 }
